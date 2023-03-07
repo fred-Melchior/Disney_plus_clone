397c1ba1ee8a5c27e5771fc3900614585caed722
fred-Melchior <122490902+fred-Melchior@users.noreply.github.com>
2023-03-07 11:02:39 -0300

    section faq

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(){
     const buttons = document.querySelectorAll('[data-tab-button]')
+    const questions = document.querySelectorAll('[data-faq-question]')
 
     for (let i =0; i < buttons.length; i++){
         buttons[i].addEventListener('click', function(botao){
@@ -12,7 +13,18 @@ document.addEventListener('DOMContentLoaded', function(){
             btn.classList.add('shows__tabs__button--is-active')
         })
     }
+
+    for(let i = 0; i < questions.length; i++){
+        questions[i].addEventListener('click', abreFecha)
+    }
 })
+
+function abreFecha(elemento){
+    const classe = 'faq__questions__item--is-open'
+    const elementoPai = elemento.target.parentNode
+
+    elementoPai.classList.toggle(classe)
+}
 
 function hideAllTabs(){
     const tabsContainer = document.querySelectorAll('[data-tab-id]')
